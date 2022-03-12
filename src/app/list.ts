c3f2afccaf2c7062cc0abb399b020b87d7768a77
Leoxdd100 <leonardohidemitsu@gmail.com>
2022-03-11 22:27:14 -0300

    Initial commit

---
+++ src/app/list.ts
@@ -0,0 +1,71 @@
+export const listData = [
+  {
+    id: 'IDS001',
+    text: 'Desenvolvimento para Servidores I',
+    date: 'Segunda-Feira',
+    hour: '9:30 - 13:00',
+    ement:
+      'PHP histórico e emprego. Instalação e configuração básica do PHP e um IDE. Sintaxe básica do PHP. Usando o PHP como um mecanismo de modelo simples. Panorama das melhores práticas com PHP. Conceitos de programação HTTP. Codificação de caracteres. Localidades, fusos horários e funções de tempo. Strings. Uso de Array e funções de matriz. Orientação a objetos em PHP (Classes, objetos, herança, encapsulamento, polimorfismo, agregação, composição e métodos). Tratamento de exceções de erro. Arquitetura do lado do servidor. Manipulação de dados postados. Enviando e-mail. Sessões e autenticação. Cookies. Arquivo manuseio e armazenamento de dados em arquivos de texto. Gerenciador de banco de dados e suas funções. Frameworks. Web Services, API, RSS, JSON e Ajax. Hospedagem compartilhada',
+    objective:
+      'Esta disciplina fornecerá uma visão geral da linguagem script PHP com associado a um gerenciador de banco de dados que utilize a linguagem SQL e como usar essas tecnologias para gerar sites dinâmicos. Introduzir práticas de codificação seguras. Objetivos específicos.  Os estudantes deverão ser capazes de desenvolver um CMS simples ou um aplicativo Web completo (lado cliente e lado servidor',
+  },
+  {
+    id: 'ISW012',
+    text: 'Desenvolvimento para Dispostivos Móveis I',
+    date: 'Segunda-Feira',
+    hour: '19:00 - 22:30',
+    ement:
+      'Introdução e conceitos. Recursos disponíveis para dispositivos móveis na linguagem de marcação de textos. Uso de algum ambiente de desenvolvimento: configuração do ambiente, emulação de dispositivos móveis nos computadores pessoais, interface gráfica, serviços disponíveis, banco de dados nos dispositivos. Desenvolvimento de aplicações que utilizam dados de geolocalização. Integração entre sistemas remotos',
+    objective:
+      'Apresentar as tecnologias de desenvolvimento de aplicativos para dispositivos móveis. Objetivos específicos.  Desenvolver aplicações para dispositivos móveis de plataformas não proprietárias',
+  },
+  {
+    id: 'AGM004',
+    text: 'Negócios e Marketing e Eletrônicos',
+    date: 'Quarta-Feira',
+    hour: '19:00 - 22:30',
+    ement:
+      'Conceitos de marketing e de marketing digital. O comportamento do consumidor on-line. Marketing de conteúdo. Marketing nas mídias sociais. E-mail marketing. Marketing viral. Publicidade on-line. Pesquisa de marketing on-line. Marketing e os negócios na Internet. Estudos de caso',
+    objective:
+      'Apresentar e discutir as questões do marketing na era da Internet. Abordar as principais estratégias, ações, ferramentas e mídias presentes na Web. Objetivos específicos. Compreender e utilizar a Internet para os negócios e implementar ações de marketing, comunicação e publicidade on-line',
+  },
+  {
+    id: 'IDS002',
+    text: 'Desenvolvimento para Servidores II',
+    date: 'Quinta-Feira',
+    hour: '19:00 - 22:30',
+    ement:
+      'Conceitos e evolução das tecnologias de programação de servidores. Recursos da linguagem escolhida para servidores na Internet. Padrões de projetos. Integração com sistemas (Google Maps API, Twitter, entre outros',
+    objective:
+      'Desenvolver um site completo de e-commerce ou outro tipo de negócio na Internet usando uma linguagem apropriada a servidores, banco de dados e padrões de projeto. Objetivos específicos. Implementar softwares do lado servidor e com uso de uma linguagem de programação e de padrões de projetos mais usuais como MVC, DAO, Composite, Singleton, entre outros',
+  },
+  {
+    id: 'ITE004',
+    text: 'Tópicos Especiais em Sistemas para Internet II',
+    date: 'Sexta-Feira',
+    hour: '19:00 - 22:30',
+    ement: 'Recursos e aplicações da tecnologia escolhida',
+    objective:
+      'Apresentar tecnologias ainda não estudadas ou aprofundar algum tema já visto definido pela coordenadoria do curso em cada unidade. Objetivos específicos. Estudar e aplicar tecnologias de uso corrente na região na qual o curso de insere ou de interesse para o desenvolvimento profissional dos estudantes ou desenvolver temas que aprofundem algum conhecimento já estudado',
+  },
+  {
+    id: 'ISW011',
+    text: 'Projeto de Prototipagem e Testes de Usabilidade',
+    date: 'Sábado',
+    hour: '8:00 - 11:30',
+    ement:
+      'Conceitos, métodos de prototipagem e ferramentas de teste de protótipos. Conceitos de usabilidade e estratégias de design. Métodos de teste de usabilidade. Planejamento de teste de usabilidade. Recrutamento. Projeto de usabilidade. Plano de teste. Sessão de teste de usabilidade. Análise de dados. Comunicação dos resultados. Variação de técnicas',
+    objective:
+      'Aprofundar os conceitos de design da usabilidade  e métodos de ensaio para mídia interativa. Usabilidade baseada na adaptação de design de interface para uma experiência do usuário mais eficaz.  Apresentar e usar diferentes métodos e ferramentas de prototipagem para explorar com os colegas de equipe ou clientes: conceitos de design, comunicação e testes de usabilidade. Objetivos específicos. Os estudantes serão capazes de criar uma série de protótipos para aplicações Web hipotéticas Criar um protótipo interativo baseando-se no protótipo de papel. Revisar protótipo. Projetar, conduzir e analisar testes de usabilidade com base: nos princípios estabelecidos, nos resultados de pesquisa com o protótipo e na teoria.  Desenvolver um plano de teste de usabilidade, recrutar os usuários apropriados, criar cenários robustos de tarefas, facilitar os testes,  analisar e comunicar os resultados do teste',
+  },
+  {
+    id: 'TTG401',
+    text: 'Projeto de Trabalho de Graduação em Sistemas para Internet',
+    date: 'Sábado',
+    hour: '11:30 - 13:10',
+    ement:
+      'Conceito de portfólio no âmbito da educação superior. Planejamento e organização de portfólios',
+    objective:
+      'Apoiar o estudante na organização de seu portfólio de projetos desenvolvidos ao longo do curso. Objetivos específicos. Os estudantes devem ser capazes de organizar o portfólio individual dos projetos desenvolvidos ao longo do curso. Devem ser capazes de redigir resumos, artigos e relatórios técnicos dos trabalhos em linguagem apropriada e na forma culta. Deve demonstrar independência de pensamento e autonomia para pesquisa',
+  },
+];
